aa881e32f27af1bf719953971d431d64217ff1c9
W-ill-c <williamcollett36@gmail.com>
2022-12-09 15:17:04 +0000

    font and laptop width  nav styling, gps icon

--- src/searchNavComponents/wholeSearch.js
+++ src/searchNavComponents/wholeSearch.js
@@ -1,12 +1,14 @@
 import React from "react";
-import SearchTitle from './weatherTitle'
-import SearchInput from './input'
+import SearchTitle from './weatherTitle';
+import SearchInput from './input';
+import GPSIcon from './gpsIcons';
 
 export default function WholeSearch(){
     return(
         <div className="locationSearch">
             <SearchTitle />
             <SearchInput />
+            <GPSIcon />
         </div>
     )
 }
