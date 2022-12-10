df49378ae38ab25f8c43e844b84af7f4e94f0794
W-ill-c <115991146+W-ill-c@users.noreply.github.com>
2022-12-10 14:29:53 +0000

    Location image comp (#2)
    * location Search input and icon with title
    
    * font and laptop width  nav styling, gps icon
    
    * css update med/large devices
    
    * locationImage without responsiveness

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import WholeSearch from './searchNavComponents/wholeSearch'
 import './searchNavComponents/search.css'
+import LocationSection from './locationImageComponents/backgroundImageSection'
+import './locationImageComponents/locationImage.css'
 
 function App() {
   return (
     <div className="App">
       <WholeSearch />
+      <LocationSection />
     </div>
   );
 }
---
+++ src/locationImageComponents/locationTitle.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function LocationTitle(){
+    return(    
+        <div className="locationTitle">
+            <h2>Location</h2>
+        </div>
+    )
+}
