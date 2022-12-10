a0833ad8964df99df936d7cde2ec70eee7c20c5d
W-ill-c <115991146+W-ill-c@users.noreply.github.com>
2022-12-10 13:54:48 +0000

    Location search (#1)
    * location Search input and icon with title
    
    * font and laptop width  nav styling, gps icon
    
    * css update med/large devices
    
    * css 992 width and up

---
+++ src/searchNavComponents/gpsIcons.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { FaLocationArrow } from "react-icons/fa";
+
+export default function GPSIcon () {
+    return(
+        <div className="locationIconDiv">
+            <FaLocationArrow className="locationIcon"/>
+        </div>
+    )
+}
