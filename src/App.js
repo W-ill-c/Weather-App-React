7e30e65568dd615245a59be0e6327bff26df8b33
W-ill-c <williamcollett36@gmail.com>
2022-12-10 13:52:16 +0000

    sun and uv-poll put in with spacing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import WholeSearch from './searchNavComponents/wholeSearch'
 import './searchNavComponents/search.css'
+import './hourlyCarouselBackground/hourlyBackground.css'
+import BackgroundMain from'./hourlyCarouselBackground/backgroundMain'
 
 function App() {
   return (
     <div className="App">
       <WholeSearch />
+      <BackgroundMain />
     </div>
   );
 }
---
+++ src/hourlyCarouselBackground/sun.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { FaSun } from "react-icons/fa";
+
+export default function SunRiseSet(){
+    return(
+        <div className="sunRiseSet">
+            <FaSun className="sunIcon"/><b>Sunrise</b> Sunset
+        </div>
+    )
+}
